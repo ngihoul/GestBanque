4eaa7e9d8737283660954f3b5b7c414672070c5f
Nicolas Gihoul <ngihoul@hotmail.com>
2024-06-27 14:21:20 +0200

    Ajoutez des fichiers projet.

---
+++ Models/Courant.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Courant
+    {
+        public string Numero { get; set; }
+        public double Solde { get; private set; }
+        public double LigneDeCredit
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    LigneDeCredit = value;
+                }
+                else
+                {
+                    throw new Exception();
+                }
+
+            }
+        }
+        public Personne Titulaire { get; set; }
+
+        public void Retrait(double Montant)
+        {
+            Solde = Montant > 0 ? Solde -= Montant : throw new Exception();
+        }
+
+        public void Depot(double Montant)
+        {
+            if (Montant > 0)
+            {
+                Solde += Montant;
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
---
+++ Models/Personne.cs
@@ -0,0 +1,9 @@
+﻿namespace Models
+{
+    public class Personne
+    {
+        public string Nom {  get; set; }
+        public string Prenom { get; set; }
+        public DateTime DateNaiss { get; set; }
+    }
+}
