c4520eb267281189f0e454986266edb2eefb5f18
Nicolas Gihoul <ngihoul@hotmail.com>
2024-07-01 10:59:03 +0200

    Création indexeur

--- GestBanque/Program.cs
+++ GestBanque/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using Models;
+
+Courant c1 = new Courant()
+{
+    Numero = "BE00 0001 0002 0003",
+};
+
+c1.Depot(2000);
+
+Courant c2 = new Courant()
+{
+    Numero = "BE00 0004 0005 0006",
+};
+
+c2.Depot(5000);
+
+Banque b = new Banque();
+b.Ajouter(c1);
+b.Ajouter(c2);
+
+Console.WriteLine(b["BE00 0001 0002 0003"].Solde);
+
---
+++ Models/Banque.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Banque
+    {
+        private Dictionary<string, Courant> _Comptes = new Dictionary<string, Courant>();
+
+        public string Nom { get; set; }
+
+        public Courant this[string key] { 
+            get {
+                _Comptes.TryGetValue(key, out Courant c);
+                return c;
+            }
+            set {
+                _Comptes[key] = value;
+            }
+        }
+
+        public void Ajouter(Courant compte) {
+            _Comptes.Add(compte.Numero, compte);
+        }
+
+        public void Supprimer(string numero) {
+            _Comptes.Remove(numero);
+        }
+    }
+}
